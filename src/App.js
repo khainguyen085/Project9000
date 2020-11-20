a394dbab397c95fbca47daa9e9b8fec4805bd56c
khainguyen085 <quochai052015@email.com>
2020-11-20 15:52:38 +0700

    set up new todoApp

--- src/App.js
+++ src/App.js
@@ -1,53 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState } from 'react';
-import {createTodoAtIndex} from './Components/createToDoAtIndex.js'
-import {handleKeyDown} from './Components/handleKeyDown.js'
-import {removeTodoAtIndex} from './Components/removeTodoAtIndex.js'
-import {toggleTodoCompleteAtIndex} from './Components/toggleTodoCompleteAtIndex.js'
-import {updateTodoAtIndex} from './Components/updateTodoAtIndex.js'
+import React from 'react'
 
 function App() {
-  const [todos, setTodos] = useState([
-    {
-      content: 'Pickup dry cleaning',
-      isCompleted: true,
-    },
-    {
-      content: 'Get haircut',
-      isCompleted: false,
-    },
-    {
-      content: 'Build a todo app in React',
-      isCompleted: false,
-    }
-  ]);
-
   return (
-    <div className="app">
-      <div className="header">
-        <img src={logo} className="logo" alt="logo" />
-      </div>
-      <form className="todo-list">
-        <ul>
-          {todos.map((todo, i) => (
-            <div className={`todo ${todo.isCompleted && 'todo-is-completed'}`}>
-              <div className={'checkbox'} onClick={() => toggleTodoCompleteAtIndex(i)}>
-                {todo.isCompleted && (
-                  <span>&#x2714;</span>
-                )}
-              </div>
-              <input
-                type="text"
-                value={todo.content}
-                onKeyDown={e => handleKeyDown(e, i)}
-                onChange={e => updateTodoAtIndex(e, i)}
-              />
-            </div>
-          ))}
-        </ul>
-      </form>
-    </div>
+   <div>
+     <h1>hello</h1>
+   </div>
   );
 }
 
