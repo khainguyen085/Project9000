775f51723240878a418823fcabe7919c3b7f6e38
khainguyen085 <quochai052015@email.com>
2020-11-20 20:24:45 +0700

    set up new app todo

--- src/App.js
+++ src/App.js
@@ -1,13 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
-import React from 'react'
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-   <div>
-     <h1>hello</h1>
-   </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //set up state
+    this.state = {
+      inputValue: "",
+      todos: []
+    }
+  }
+  inputChange = (e) => {
+    this.setState({
+      inputValue: e.target.value,
+    });
+  }
+  buttonSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.inputValue !== "") { // validation
+      this.setState({
+        todos: [this.state.inputValue, ...this.state.todos],
+        inputValue: "",
+      });
+    }
+  };
+  buttonDone =(e) => {
+    e.preventDefault();
+    console.log('click');
+  }
+  buttonDelete =(e) =>{
+    console.log('delete');
+    
+  }
+  render() {
+    return (
+      <>
+        <div className='container'> 
+          <h1>TODO APP</h1> <br/>
+          <div className='miniContainer'>
+          <button>SignIn</button>
+          <button>SignUp</button>
+          </div>
+          <form className='smallContainer' onSubmit={this.buttonSubmit}>
+            <input onChange={this.inputChange} value={this.state.inputValue} type="text" placeholder="Nhập nhiệm vụ...." />
+            <button onClick={this.buttonSubmit}>Add</button>
+            <ol>
+              {this.state.todos.map((todo, index) => (
+                <div>
+                  <li key={index}>{todo}</li>
+                  <button onClick={this.buttonDelete}>Xoá</button>
+                  <button onClick={this.buttonDone}>Xong</button>
+                </div>
+              ))}
+            </ol>
+          </form>
+        </div>
+      </>
+    );
+  }
 }
-
-export default App;
