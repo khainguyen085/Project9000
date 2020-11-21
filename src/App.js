b96bc4ab3ff4de9ab4fa3600da01654544b304cd
khainguyen085 <quochai052015@email.com>
2020-11-21 21:30:20 +0700

    new app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
+import React from 'react'
+import {Seachbar} from './Components/Seachbar'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state={}
+    this.handleSub=this.handleSub.bind(this)
+  }
+  handleSub(e){
+    e.preventDefault()
+    console.log('hello')
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Weather App</h1>
+        <form>
+          <Seachbar/>
+          <button onChange={this.handleSub} type='submit'>Search</button>
+        </form>
+      </div>
+    );
+  }
 }
-
-export default App;
---
+++ src/Components/Seachbar.jsx
@@ -0,0 +1,4 @@
+import React from 'react'
+export function Seachbar(){
+    return <input type='text' placeholder='what are u looking for'/>
+}
