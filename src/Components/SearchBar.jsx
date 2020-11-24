57515a1ad8c13a65e87c644a0309579724ebb08b
khainguyen085 <quochai052015@email.com>
2020-11-24 19:51:26 +0700

    set up new app BookFinder

---
+++ src/Components/SearchBar.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default class SearchBar extends React.Component{
+    constructor(props){
+        super(props)
+        this.state = {
+            Books:[]
+        }
+        
+    }
+}
