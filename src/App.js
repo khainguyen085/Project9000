ca950b79d8d2a715b46767440c487341e2d73f93
khainguyen085 <quochai052015@email.com>
2020-11-25 17:36:11 +0700

    update

--- src/App.js
+++ src/App.js
@@ -1,29 +1,11 @@
 import './App.css';
 import React from 'react'
-import {Seachbar} from './Components/Seachbar'
-
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state={}
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleKeyDown = this.handleKeyDown.bind(this)
-  }
-  handleSubmit(){
-    console.log('hello')
-  }
-  handleKeyDown(){
-    console.log('done')
-  }
-  render() {
-    return (
-      <div className="App">
-        <h1>Weather App</h1>
-        <form>
-          <Seachbar handleKeyDown={this.handleKeyDown} />
-          <button onChange={this.handleSubmit} type='submit'>Search</button>
-        </form>
-      </div>
-    );
-  }
+import WeatherList from  './Components/WeatherList'
+import Header from './Components/Header'
+export default function App(){
+  return(
+    <>
+      <Header/>
+    </>
+  )
 }
--- src/Components/Seachbar.jsx
+++
@@ -1,4 +0,0 @@
-import React from 'react'
-export function Seachbar(props){
-    return <input type='text' placeholder='what are u looking for' onKeyDown={props.handleKeyDown}/>
-}
