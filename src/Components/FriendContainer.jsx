25c93197b3ce404158c78d21bbefcb0a76dbbbbb
khainguyen085 <quochai052015@email.com>
2020-11-28 12:08:26 +0700

    setup failed

---
+++ src/Components/FriendContainer.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import {Friends} from './Friends'
+export default function FriendContainer(){
+    return(
+        <>
+            <Friends data={name}/>
+        </>
+    )
+}
