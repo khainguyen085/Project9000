16ea93b105c69e5106b890a25942ab617e3e84dd
khainguyen085 <quochai052015@email.com>
2020-11-28 20:55:19 +0700

    temp

---
+++ src/Components/Charity.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+export default function Charity(props){
+    return(
+        <div className='CharityContainer'>
+            //
+        </div>
+    )
+}
---
+++ src/Components/CharityList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Charity from './Charity'
+export default class CharityList extends React.Component{
+    constructor(props){
+        super(props);
+        
+    }
+    
+    render(){
+        return (
+
+        ) 
+    }
+}
