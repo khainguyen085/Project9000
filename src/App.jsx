ecdbdbe3b8f99ffd349d3574d25d3ab3dc0f08a2
khainguyen085 <quochai052015@email.com>
2020-12-08 14:57:24 +0700

    update

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+// import logo from './logo.svg';
+import './App.css';
+import React from 'react'
+import "bootstrap/dist/css/bootstrap.min.css"
+
+export default class App extends React.Component{
+  render(){
+    return(
+      <h1>
+        hello
+      </h1>
+    )
+  }
+}
